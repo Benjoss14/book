8f820f71b977c5670cffdad4605c5af557e98c7f
unknown <benjamin.lefeuvre28@gmail.com>
2021-02-07 16:30:12 +0100

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,120 @@
+var a=1;
+//function digi()
+//{
+//    if(a==1)
+//      {
+//        document.getElementById("digital2").style.display="block";
+//        return a=0;
+//    }
+//
+//    else {
+//        document.getElementById("digital2").style.display="none";
+//        return a=1;
+//    }
+//}
+
+
+
+
+$(document).ready(function(){
+  $(".digiii").click(function(){
+    $("#digital2").slideDown();
+  });
+});
+
+$(document).ready(function(){
+  $(".digiiii").click(function(){
+    $("#digital2").slideUp();
+  });
+});
+
+function myFunction() {
+    document.getElementById('digital').style.cssText = 'color: var(--brown);';
+    document.getElementById('digital').style.css = 'margin-top: -60px;';
+}
+
+function myFunction2() {
+    document.getElementById('digital').style.cssText = 'color: var(--white);';
+}
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+  $(".tradiii").click(function(){
+    $("#tradi2").slideDown();
+  });
+});
+
+$(document).ready(function(){
+  $(".tradiiii").click(function(){
+    $("#tradi2").slideUp();
+  });
+});
+
+function myFunction3() {
+    document.getElementById('tradi').style.cssText = 'color: var(--brown);';
+}
+
+function myFunction4() {
+    document.getElementById('tradi').style.cssText = 'color: var(--white);';
+}
+
+
+
+
+
+$(document).ready(function(){
+  $(".photoshoppp").click(function(){
+    $("#photoshop2").slideDown();
+  });
+});
+
+$(document).ready(function(){
+  $(".photooo").click(function(){
+    $("#photoshop2").slideUp();
+
+  });
+});
+
+function myFunction5() {
+    document.getElementById('photoshop').style.cssText = 'color: var(--brown);';
+    document.getElementById('photoshop').style.css = 'margin-top: 60px;';
+}
+
+function myFunction6() {
+    document.getElementById('photoshop').style.cssText = 'color: var(--white);';
+}
+
+
+
+
+
+
+
+$(document).ready(function(){
+  $(".illuuu").click(function(){
+    $("#illustrator2").slideDown();
+  });
+});
+
+$(document).ready(function(){
+  $(".illuuuu").click(function(){
+    $("#illustrator2").slideUp();
+  });
+});
+
+
+function myFunction7() {
+    document.getElementById('illustrator').style.cssText = 'color: var(--brown);';
+    document.getElementById('illustrator').style.css = 'margin-top: 60px;';
+}
+
+function myFunction8() {
+    document.getElementById('illustrator').style.cssText = 'color: var(--white);';
+
+}
